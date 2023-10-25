af4e0febe367ccd45358130c7465be6afd556cfb
Kanavaar <103877398+Kanavaar@users.noreply.github.com>
2023-10-25 20:58:17 +0000

    http-rs(lib): Result<T> => HttpResult<T>

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ pub mod method;
 pub use crate::error::Error;
 pub use crate::method::Method;
 
-pub type Result<T> = core::result::Result<T, Error>;
+pub type HttpResult<T> = core::result::Result<T, Error>;
